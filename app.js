0d4ffb520851c7bc4ce4aaaa065d2f2db9430ebb
okHadi <khan.hadi2951@gmail.com>
2022-07-30 00:50:10 +0500

    init commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 4301;
+
+app.listen(port, () => { console.log("Node Server Started at PORT: " + port); });
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
